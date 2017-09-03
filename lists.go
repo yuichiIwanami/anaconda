9da1e5cd1d08de878885111d10c82be766cd1dd8
yuichi-asus <gaudi8@gmail.com>
2017-09-03 20:31:50 +0900

    ShowList(), AddListUser(), RemoveListUser(), GetListUsers()

--- lists.go
+++ lists.go
@@ -16,16 +16,45 @@ func (a TwitterApi) CreateList(name, description string, v url.Values) (list Lis
 	return list, (<-response_ch).err
 }
 
+func (a TwitterApi) ShowList(name, description string, v url.Values) (list List, err error) {
+	if v == nil {
+		v = url.Values{}
+	}
+	v.Set("name", name)
+	v.Set("description", description)
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/lists/create.json", v, &list, _POST, response_ch}
+	return list, (<-response_ch).err
+}
+
 // AddUserToList implements /lists/members/create.json
-func (a TwitterApi) AddUserToList(screenName string, listID int64, v url.Values) (users []User, err error) {
-	v = cleanValues(v)
+func (a TwitterApi) AddListUser(userID int64, listID int64, v url.Values) (users []User, err error) {
+//func (a TwitterApi) AddUserToList(screenName string, listID int64, v url.Values) (users []User, err error) {
+	if v == nil {
+		v = url.Values{}
+	}
 	v.Set("list_id", strconv.FormatInt(listID, 10))
-	v.Set("screen_name", screenName)
+	v.Set("user_id", strconv.FormatInt(userID, 10))
 
 	var addUserToListResponse AddUserToListResponse
 
 	response_ch := make(chan response)
-	a.queryQueue <- query{a.baseUrl + "/lists/members/create.json", v, &addUserToListResponse, _POST, response_ch}
+	a.queryQueue <- query{BaseUrl + "/lists/members/create.json", v, &addUserToListResponse, _POST, response_ch}
+	return addUserToListResponse.Users, (<-response_ch).err
+}
+
+func (a TwitterApi) RemoveListUser(usrID int64, listID int64, v url.Values) (users []User, err error) {
+	if v == nil {
+		v = url.Values{}
+	}
+	v.Set("list_id", strconv.FormatInt(listID, 10))
+	v.Set("user_id", strconv.FormatInt(usrID, 10))
+
+	var addUserToListResponse AddUserToListResponse
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/lists/members/destroy.json", v, &addUserToListResponse, _POST, response_ch}
 	return addUserToListResponse.Users, (<-response_ch).err
 }
 
@@ -50,4 +79,9 @@ func (a TwitterApi) GetListTweets(listID int64, includeRTs bool, v url.Values) (
 	response_ch := make(chan response)
 	a.queryQueue <- query{a.baseUrl + "/lists/statuses.json", v, &tweets, _GET, response_ch}
 	return tweets, (<-response_ch).err
+}
+
+func (a TwitterApi) GetListUsers(v url.Values) (c UserCursor, err error) {
+	err = a.apiGet(BaseUrl+"/lists/members.json", v, &c)
+	return
 }
